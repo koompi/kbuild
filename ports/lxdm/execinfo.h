8d80d05d99fd6b01e50f01a8cf02fde00eb9819b
hangsiahong <honghangsia@gmail.com>
2021-02-27 12:13:55 +0700

    update

---
+++ ports/lxdm/execinfo.h
@@ -0,0 +1,19 @@
+#ifndef _EXECINFO_H
+#define _EXECINFO_H 1
+#include <features.h>
+__BEGIN_DECLS
+/* Store up to SIZE return address of the current program state in
+ *    ARRAY and return the exact number of values stored.  */
+extern int backtrace (void **__array, int __size) __nonnull ((1));
+/* Return names of functions from the backtrace list in ARRAY in a newly
+ *    malloc()ed memory block.  */
+extern char **backtrace_symbols (void *const *__array, int __size)
+	     __THROW __nonnull ((1));
+	     /* This function is similar to backtrace_symbols() but it writes the result
+	      *    immediately to a file.  */
+	     extern void backtrace_symbols_fd (void *const *__array, int __size, int __fd)
+	     __THROW __nonnull ((1));
+	     __END_DECLS
+#endif /* execinfo.h  */
+
+
